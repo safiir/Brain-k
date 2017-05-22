3a101dd1b339bccedcec8b37ab40588bee55857c
sapphire1317 <809320935@qq.com>
2017-05-23 02:07:56 +0800

    do nothing

--- bf.rb
+++ bf.rb
@@ -9,98 +9,96 @@ code = nil
 if ARGV.size > 0
   code = File.read(ARGV[0])
 else
-  puts "BF source file is missing!"
+  puts "Brain_K source file is missing!"
   exit
 end
 
-class BF
+class Brain_K
 
-  attr_reader :ds
-  
   @@TOKENS = ['+', '-', '<', '>', '.', ',', '[', ']']
   
   def initialize(code)
-    @ds = []
-    @dp = 0
-    @cs = code.chars.select{|c|@@TOKENS.include?(c)}
-    @cp = 0
-    @ss = []
+    @data_segment = []
+    @data_segment_point = 0
+    @code_segment = code.chars.select{|c|@@TOKENS.include?(c)}
+    @code_segment_point = 0
+    @stack_segment = []
   end
   
-  def add
-    if @ds[@dp]
-      @ds[@dp] += 1
+  def ins
+    if @data_segment[@data_segment_point]
+      @data_segment[@data_segment_point] += 1
     else
-      @ds[@dp] = 1
+      @data_segment[@data_segment_point] = 1
     end
   end
 
-  def sub
-    if @ds[@dp]
-      @ds[@dp] -= 1
+  def dec
+    if @data_segment[@data_segment_point]
+      @data_segment[@data_segment_point] -= 1
     else
-      @ds[@dp] = 0
+      @data_segment[@data_segment_point] = 0
     end
   end
 
-  def right
-    @dp += 1
+  def left_shift
+    if @data_segment_point > 0
+      @data_segment_point -= 1
+    end
+  end
+
+  def right_shift
+    @data_segment_point += 1
   end 
 
-  def left
-    if @dp > 0
-      @dp -= 1
-    end
+  def put_char
+    print @data_segment[@data_segment_point]
   end
 
-  def putchar
-    print @ds[@dp]
+  def get_char
+    @data_segment[@data_segment_point] = STDIN.gets.to_i
   end
 
-  def getchar
-    @ds[@dp] = STDIN.gets.to_i
-  end
-
-  def start
-    if @ds[@dp] != 0
-      @ss << @cp
+  def left_parenthesis
+    if @data_segment[@data_segment_point] != 0
+      @stack_segment << @code_segment_point
     else
       point = nil
-      (@cp + 1).upto(@cs.size - 1) do |i|
-        if @cs[i] == @@TOKENS[7]
+      (@code_segment_point + 1).upto(@code_segment.size - 1) do |i|
+        if @code_segment[i] == @@TOKENS[7]
           point = i + 1
           break
         end
       end
-      @cp = point
+      @code_segment_point = point
     end
   end
 
-  def stop
-    if @ds[@dp] != 0
-      @cp = @ss.pop - 1
+  def right_parenthesis
+    if @data_segment[@data_segment_point] != 0
+      @code_segment_point = @stack_segment.pop - 1
     else
-      @ss.pop
+      @stack_segment.pop
     end
   end
 
   def run
-    while @cs[@cp] 
-      case @cs[@cp]
-        when @@TOKENS[0] then add
-        when @@TOKENS[1] then sub
-        when @@TOKENS[2] then left
-        when @@TOKENS[3] then right
-        when @@TOKENS[4] then putchar
-        when @@TOKENS[5] then getchar
-        when @@TOKENS[6] then start
-        when @@TOKENS[7] then stop
+    while @code_segment[@code_segment_point] 
+      case @code_segment[@code_segment_point]
+        when @@TOKENS[0] then ins
+        when @@TOKENS[1] then dec
+        when @@TOKENS[2] then left_shift
+        when @@TOKENS[3] then right_shift
+        when @@TOKENS[4] then put_char
+        when @@TOKENS[5] then get_char
+        when @@TOKENS[6] then left_parenthesis
+        when @@TOKENS[7] then right_parenthesis
       end
-      @cp += 1
+      @code_segment_point += 1
     end
     puts
   end
 
 end
 
-BF.new(code).run
+Brain_K.new(code).run
